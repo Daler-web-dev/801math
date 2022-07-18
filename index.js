290060fe82fee6f43f1572f25fc38a15cdf9219f
Daler  Sharifkulov <dsarifkulo021@gmail.com>
2022-07-18 21:05:11 +0500

    h1 was added to code

---
+++ index.js
@@ -0,0 +1,57 @@
+// методы цифр 
+// github
+
+
+// string
+// let str = "hello"
+
+// str.slice(ot, do)
+// str.search('o') // index
+// str.trim()
+// str.toLowerCase()
+// str.toUpperCase()
+// str.split(' ')
+// str.replace('hello', 'bye')
+// str.includes('l') // booelan
+// str.length
+// str.charAt()
+// str[0]
+// str.replaceAll('hello', 'bye')
+
+
+// number
+
+let int = 10
+let int_two = 12
+
+// String()
+// toString()
+// toFixed()
+
+console.log(
+    // Math.floor(int) // down
+    // Math.ceil(int) // up
+    // Math.round(int) // logical
+
+    // parseInt(int) // parse to number from string
+    // parseFloat(int) // parse to number from string float
+    // +int // parse to number from string float
+    
+    // Math.max(int, int_two, 88,1080)
+    // Math.min(int, int_two, 88,1080)
+    // Math.pow(7,2)
+    // **
+
+    // Math.floor(Math.random() * 100)
+
+    // int > int_two
+
+    // int + 20
+
+);
+
+
+
+
+// git config -g user.user "Alex Adams"
+// git config -g user.email "alex@gmail.com"
